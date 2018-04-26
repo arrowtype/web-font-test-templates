2710ae26d0a129de75787363c6b3de8ff75bbbb0
Stephen Nixon <swr410@gmail.com>
2018-04-26 12:39:55 +0200

    make matrix update on each letter input

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,44 +6,44 @@ const fontSectionLinks = document.querySelectorAll("#sidebar a")
 
 function highlightNav() {
     // console.log(window.scrollY)
-    console.log(fontSections);
-        for (var section of fontSections) {
-            // console.log("scroll is ", window.scrollY)
-            let scrollPos = section.offsetTop - window.scrollY
-            let bottomPos = scrollPos + section.offsetHeight 
+    // console.log(fontSections);
+    for (var section of fontSections) {
+        // console.log("scroll is ", window.scrollY)
+        let scrollPos = section.offsetTop - window.scrollY
+        let bottomPos = scrollPos + section.offsetHeight 
 
-            console.log(section.offsetTop)
-            if (scrollPos <= 100 && scrollPos + section.offsetHeight >= 100) {
-                
-                for (var link of fontSectionLinks){
-                    let linkHref = link.hash.replace('#','');
-                    // console.log(linkHref);
-                    if (linkHref == section.id) {
-                        // console.log(link.hash)
-                        link.classList.add("highlight")
-                    }
-                }
-            }
-            else if (scrollPos >= 100 || bottomPos < 100) {
-                // console.log(section.id, scrollPos, bottomPos, "out of view")
-                for (var link of fontSectionLinks){
-                    let linkHref = link.hash.replace('#','');
-                    // console.log(linkHref);
-                    if (linkHref == section.id) {
-                        // console.log(link.hash)
-                        link.classList.remove("highlight")
-                    }
+        // console.log(section.offsetTop)
+        if (scrollPos <= 100 && scrollPos + section.offsetHeight >= 100) {
+            
+            for (var link of fontSectionLinks){
+                let linkHref = link.hash.replace('#','');
+                // console.log(linkHref);
+                if (linkHref == section.id) {
+                    // console.log(link.hash)
+                    link.classList.add("highlight")
                 }
             }
         }
+        else if (scrollPos >= 100 || bottomPos < 100) {
+            // console.log(section.id, scrollPos, bottomPos, "out of view")
+            for (var link of fontSectionLinks){
+                let linkHref = link.hash.replace('#','');
+                // console.log(linkHref);
+                if (linkHref == section.id) {
+                    // console.log(link.hash)
+                    link.classList.remove("highlight")
+                }
+            }
+        }
+    }
 }   
 
 function addTypeSpecListeners(){
     highlightNav()
     document.addEventListener('scroll', highlightNav)
-    for (var link of fontSectionLinks) {
-        link.addEventListener('onclick', highlightNav)
-    }
+    // for (var link of fontSectionLinks) {
+    //     link.addEventListener('onclick', highlightNav)
+    // }
 }
 
 window.addEventListener('load', addTypeSpecListeners);
@@ -72,7 +72,7 @@ function typeTestMatrix() {
 // typeTesterInput.addEventListener('change', typeTestMatrix);
 
 function addMatrixListeners(){
-    typeTesterInput.addEventListener('change', typeTestMatrix);
+    typeTesterInput.addEventListener('input', typeTestMatrix);
 }
 
 window.addEventListener('load', addMatrixListeners);
