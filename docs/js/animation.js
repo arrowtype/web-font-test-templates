41a5eda9fa254165a09cb35c4d35426521e2d199
Stephen Nixon <swr410@gmail.com>
2018-05-03 23:26:09 +0200

    color tweak

--- docs/js/animation.js
+++ docs/js/animation.js
@@ -10,19 +10,14 @@ basicTimeline
     duration: 1750,
     stroke: [
       {value: '#0f0'},
-      {value: 'rgba(20,20,20,0.25)'}
+      {value: 'rgba(20,20,20)'}
     ],
+    
     delay: function(el, i) { return i * 250 },
     direction: 'alternate',
     loop: true
   })
-  .add({
-    targets: '#lineDrawing g',
-    fill: ["rgba(0,0,0,0)","rgba(20,20,20,0.5)"],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    offset: '-=2000'
-  })
+  
   .add({
     targets: '.greeting',
     opacity: [0,1],
@@ -36,4 +31,14 @@ basicTimeline
     easing: 'easeInOutSine',
     duration: 750,
     offset: '-=100'
+  })
+  .add({
+    targets: '#lineDrawing g',
+    fill: ["rgba(0,0,0,0)","rgba(10,20,10,0.5)"],
+    strokeWidth: [
+      ".5",
+      "0"],
+    easing: 'easeInOutSine',
+    duration: 2500,
+    offset: '-=2000'
   })
