c640ffb7d7174437205de0b2accdeec8f0e94142
Stephen Nixon <swr410@gmail.com>
2018-05-03 22:41:11 +0200

    retaining animation from merge

--- docs/js/animation.js
+++ docs/js/animation.js
@@ -1,24 +1,4 @@
-// var lineDrawing = anime({
-//     targets: '#lineDrawing .lines path',
-//     strokeDashoffset: [anime.setDashoffset, 0],
-//     easing: 'easeInOutSine',
-//     duration: 2000,
-//     stroke: [
-//       {value: '#0f0'},
-//       {value: '#121212'}
-//     ],
-//     delay: function(el, i) { return i * 750 },
-//     direction: 'alternate',
-//     loop: true
-//   });
 
-  // var greeting = anime({
-  //   targets: '.greeting',
-  //   opacity: [0, 1],
-  //   duration: 2000;
-  //   delay: 500
-  // });
-  
 
 var basicTimeline = anime.timeline();
 
@@ -27,7 +7,7 @@ basicTimeline
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
     easing: 'easeInOutSine',
-    duration: 1000,
+    duration: 1250,
     stroke: [
       {value: '#0f0'},
       {value: '#121212'}
