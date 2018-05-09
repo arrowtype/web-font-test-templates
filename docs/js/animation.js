f91ee01e58a5a526d1cccb843e6b4e2e9b3864ba
Stephen Nixon <swr410@gmail.com>
2018-05-09 20:24:23 +0200

    messing with animation

--- docs/js/animation.js
+++ docs/js/animation.js
@@ -1,8 +1,16 @@
 
 
-var basicTimeline = anime.timeline();
+var basicTimeline = anime.timeline({
+  direction: 'alternate',
+  loop: true
+});
 
 basicTimeline
+  .add({
+    targets: '#lineDrawing .lines path',
+    duration: 1,
+    delay: 500
+  })
   .add({
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
@@ -17,20 +25,15 @@ basicTimeline
     direction: 'alternate',
     loop: true
   })
-  
   .add({
-    targets: '.greeting',
-    opacity: [0,1],
+    targets: '#lineDrawing svg',
+    fill: ["rgba(0,0,0,0)","rgba(10,40,10,0.5)"],
+    rotate: -180,
+    loop: true,
     easing: 'easeInOutSine',
-    duration: 1500,
-    offset: '-=1500'
-  })
-  .add({
-    targets: '.greeting code:first-of-type',
-    color: ['#fff',"#0f0"],
-    easing: 'easeInOutSine',
-    duration: 750,
-    offset: '-=100'
+    duration: 1000,
+    delay: 2000,
+    offset: '-=2000'
   })
   .add({
     targets: '#lineDrawing g',
@@ -41,4 +44,28 @@ basicTimeline
     easing: 'easeInOutSine',
     duration: 2500,
     offset: '-=2000'
+  })
+  .add({
+    targets: '#lineDrawing .lines path',
+    duration: 1,
+    delay: 2500
+  })
+
+var wordsTimeline = anime.timeline();
+
+wordsTimeline
+  .add({
+    targets: '.greeting',
+    opacity: [0,1],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    // offset: '=1500'
+    delay: 1000,
+  })
+  .add({
+    targets: '.greeting code:first-of-type',
+    color: ['#fff',"#0f0"],
+    easing: 'easeInOutSine',
+    duration: 750,
+    offset: '-=100'
   })
